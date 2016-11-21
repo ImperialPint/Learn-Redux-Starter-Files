ef757afcac8264cd69f61219934053b38db7a53c
Andrew Crist <andrew.l.crist@gmail.com>
2016-11-21 09:45:44 -0600

    Text

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,4 +5,4 @@ import { render } from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'))
+render(<p>hello this is a major test. please work.</p>, document.getElementById('root'))
