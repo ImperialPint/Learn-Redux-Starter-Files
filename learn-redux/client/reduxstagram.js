c966c708254b3822c72c3879bd024d0ec07be8e5
Andrew Crist <andrew.l.crist@gmail.com>
2016-11-22 13:45:15 -0600

    Test

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,4 +5,20 @@ import { render } from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
-render(<p>hello this is a major test. please work.</p>, document.getElementById('root'))
+import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}>
+        <Route path='/view/:postId' component={Single}></Route>
+      </IndexRoute>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
